ea13741ee2c742d0b26cd4fcaf75f8d0ab0da8ba
Matheus <matheus.augustoreal@gmail.com>
2022-02-01 21:25:44 -0300

    Learn how to set and update objects

--- scripts/app.js
+++ scripts/app.js
@@ -6,7 +6,9 @@ const firebaseConfig = {
     messagingSenderId: "1045228011584",
     appId: "1:1045228011584:web:42a4408bc47d5db10c5fa5",
     measurementId: "G-CW3BCGEK8Y"
-  };
+};
+
+const TURMA = "turmaA"
 
 firebase.initializeApp(firebaseConfig)
 
@@ -28,9 +30,45 @@ const db = firebase.firestore()
 //     console.log(aluno.nome)
 // }) 
 
-db.collection("turmaA").where("nome", "==", "JooJ").get().then(snapshot => {
-    snapshot.forEach(doc => {
-        let aluno = doc.data()
-        console.log(aluno.nome)
-    })
+// // Buscando documentos
+// db.collection("turmaA").where("nome", "==", "JooJ").get().then(snapshot => {
+//     snapshot.forEach(doc => {
+//         let aluno = doc.data()
+//         console.log(aluno.nome)
+//     })
+// })
+
+// Adicionar objetos na coleção
+// db.collection(TURMA).add({
+//     nome: "Marcos",
+//     sobrenome: "Santos",
+//     notas: {nota1: 9.6, nota2: 7.5}
+// }).then(doc => {
+//     console.log("Documento inserido com sucesso", doc)
+// }).catch(err => {
+//     console.log(err)
+// })
+
+// Set insere objetos no banco de dados e sobrescreve (merge não deixa sobrescrever dados)
+// db.collection(TURMA).doc("AlunoNovo").set({
+//     nome: "Mariana",
+//     sobrenome: "Oliveira",
+//     notas: { nota1: 8.6, nota2: 7.5 }
+// }, {merge: true}).then(() => {
+//     console.log("Documento inserido com sucesso")
+// }).catch(err => {
+//     console.log(err)
+// })
+
+// Update atualiza / adiciona o objeto do banco de dados
+db.collection(TURMA).doc("AlunoNovo").update({
+    sobrenome: "Lima",
+    "notas.nota1": 9,
+    "notas.nota1": firebase.firestore.FieldValue.increment(1),
+    cidades: firebase.firestore.FieldValue.arrayUnion("Rio de Janeiro"), // Adiciona elemento no array sem sobrescrever os dados
+    cidades: firebase.firestore.FieldValue.arrayRemove("Rio de Janeiro") // Remove elemento no array sem sobrescrever os dados
+}).then(() => {
+    console.log("Documento inserido com sucesso")
+}).catch(err => {
+    console.log(err)
 })
